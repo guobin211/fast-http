e1b6651805a2a57e5444b97e0cc261cc7a5b6a14
GuoBin <844922881@qq.com>
2019-07-30 15:18:48 +0800

    feat(fast-http): fast-http具体实现

--- src/fast-http.ts
+++ src/fast-http.ts
@@ -1,8 +1,28 @@
 /**
  * FastHttp
  */
-export default class FastHttp {
+import { FastPromise, FastRequestConfig, Http } from './interface/Http'
+import { Options } from './options'
+
+export default class FastHttp implements Http {
+    private _defaultOptions: Options
     constructor() {
-        // todo init fn
+        this._defaultOptions = new Options()
     }
+
+    delete(): any {}
+
+    get(): any {}
+
+    head(): any {}
+
+    options(): any {}
+
+    patch<T = any>(url: string, data?: any, config?: FastRequestConfig, options?: Options): any {}
+
+    post<T = any>(url: string, data?: any, config?: FastRequestConfig, options?: Options): any {}
+
+    put<T = any>(url: string, data?: any, config?: FastRequestConfig, options?: Options): any {}
+
+    request(config: FastRequestConfig, options?: Options): any {}
 }
